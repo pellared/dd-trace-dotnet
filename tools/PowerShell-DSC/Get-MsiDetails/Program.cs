d2f7e498d00d5f420e2afcd750a6a2d5369e70d3
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2021-04-27 13:38:20 -0400

    add console app

---
+++ tools/PowerShell-DSC/Get-MsiDetails/Program.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace GetMsiDetails
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine("Usage: Get-MsiDetails.exe <path_to_msi_package>");
+                return;
+            }
+
+            string[] propertyNames =
+            {
+                "ProductName",
+                "ProductCode",
+                "ProductVersion"
+            };
+
+            MsiPackage msi = null;
+
+            try
+            {
+                msi = new MsiPackage(args[0]);
+
+                foreach (string propertyName in propertyNames)
+                {
+                    string propertyValue = msi.GetPackageProperty(propertyName);
+                    Console.WriteLine($"{propertyName} = {propertyValue}");
+                }
+            }
+            finally
+            {
+                msi?.Dispose();
+            }
+        }
+    }
+}
