1c4bf7b5072b6831b199328b17e63c48b7dda905
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2021-09-13 16:37:55 -0400

    remove unused references to Datadog.Trace.Logging

--- tracer/src/Datadog.Trace/AsyncLocalScopeManager.cs
+++ tracer/src/Datadog.Trace/AsyncLocalScopeManager.cs
@@ -3,14 +3,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2017 Datadog, Inc.
 // </copyright>
 
-using System;
-using Datadog.Trace.Logging;
-
 namespace Datadog.Trace
 {
     internal class AsyncLocalScopeManager : ScopeManagerBase
     {
-        private readonly AsyncLocalCompat<Scope> _activeScope = new AsyncLocalCompat<Scope>();
+        private readonly AsyncLocalCompat<Scope> _activeScope = new();
 
         public override Scope Active
         {
