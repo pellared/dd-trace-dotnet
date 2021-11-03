ce01f4ee152fe1a3e143fc3d813d1fcea33678a1
Pierre Bonet <pierre.bonet@datadoghq.com>
2021-11-03 15:58:32 +0100

    Use out.host and out.port in tags

--- tracer/src/Datadog.Trace/Tags.cs
+++ tracer/src/Datadog.Trace/Tags.cs
@@ -451,7 +451,5 @@ namespace Datadog.Trace
         internal const string CouchbaseOperationCode = "couchbase.operation.code";
         internal const string CouchbaseOperationBucket = "couchbase.operation.bucket";
         internal const string CouchbaseOperationKey = "couchbase.operation.key";
-        internal const string CouchbaseOperationHost = "couchbase.operation.host";
-        internal const string CouchbaseOperationPort = "couchbase.operation.port";
     }
 }
